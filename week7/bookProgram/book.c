b95ce72096a8d295f92c4fbc26219b9c134d2e62
Corey Barr <109260671+coreybigboi@users.noreply.github.com>
2023-08-19 16:58:28 +1000

    Update book.c

--- week7/bookProgram/book.c
+++ week7/bookProgram/book.c
@@ -1,1 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "book.h"
 
+// TODO: implement the function for creating a new Book
+Book* createBook(char* title, char* author, int publicationYear) {
+    // step 1: create a new book using malloc
+    // step 2: set the fields of the new book using the function arguments
+    // step 3: return the pointer to the new book
+}
+
+// TODO: implement the function for displaying book information (title, author, year)
+void displayBook(Book* book) {
+}
+
+// Implement function to free memory allocated for a Book
+void freeBook(Book* book) {
+}
